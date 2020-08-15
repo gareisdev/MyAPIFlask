1a3055d05e7063dc5f0337299b5eb0ac7ee732e9
LeonelDGG <leonelgareis@protonmail.ch>
2020-08-15 17:38:42 -0300

    New routes defined

--- main.py
+++ main.py
@@ -1,10 +1,44 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello world!'
+
+@app.route('/tasks')
+@app.route('/tasks/<int:id_task>')
+def index(id_task=None):
+    if not id_task:
+        return jsonify({"code": 200, "message": "success"})
+    else:
+        return jsonify({"code": 200, "message": "success", "id_task": id_task})
+
+
+@app.route("/tasks/add/", methods=['POST'])
+def add_task():
+    new_task = {
+        "task": request.json['task'],
+        "status": request.json['status'],
+    }
+
+    return jsonify({
+        "status": "success",
+        "message": "task received",
+        "task": new_task
+    })
+
+
+@app.route('/tasks/update/<int:id_task>', methods=['PUT'])
+def update_task(id_task):
+    new_task = {
+        "task": request.json['task'],
+        "status": request.json['status'],
+    }
+    return jsonify({"code": 200, "message": "Task update successfully", "update": new_task})
+
+
+@app.route('/tasks/delete/<int:id_task>', methods=['DELETE'])
+def delete_task(id_task):
+    return jsonify({"code": 200, "message": "Task delete successfully", "delete": id_task})
+
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
