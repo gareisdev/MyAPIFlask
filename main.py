3bd33e301784b9153acdb0a8b9d23aa01271d2bf
LeonelDGG <leonelgareis@protonmail.ch>
2020-08-21 20:34:41 -0300

    GPL-3 license added :D

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+'''
+This program is free software: you can redistribute it and/or   modify it under the terms of the GNU General Public License aspublished bythe Free Software Foundation, either version 3 of the License, or(at your option) any later version.This program is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY; without even the implied warranty ofMERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See theGNU General Public License for more details.You should have received a copy of the GNU General Public Licensealong with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
 from flask import Flask, jsonify, request, Response
 from flask_pymongo import PyMongo
 from bson import json_util
